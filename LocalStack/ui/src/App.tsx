93aa8f8fbab15240d5e245dd5a4dca0747f0fb75
Pive01 <lucapivetta01@gmail.com>
2022-09-15 11:58:54 +0200

    First version of UI

--- LocalStack/ui/src/App.tsx
+++ LocalStack/ui/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import { createDockerDesktopClient } from '@docker/extension-api-client';
-import { Stack, TextField, Typography } from '@mui/material';
+import { Box, Card, TextField } from '@mui/material';
 
 // Note: This line relies on Docker Desktop's presence as a host application.
 // If you're running this React app in a browser, it won't work properly.
@@ -12,42 +12,72 @@ function useDockerDesktopClient() {
 }
 
 export function App() {
-  const [response, setResponse] = React.useState<string>();
+  const [status, setStatus] = useState<string>('');
   const ddClient = useDockerDesktopClient();
 
-  const fetchAndDisplayResponse = async () => {
-    const result = await ddClient.extension.vm?.service?.get('/hello');
-    setResponse(JSON.stringify(result));
-  };
+  const checkStatus = async () => {
+    const result = await ddClient.docker.cli.exec('run', ['--rm', '-i', '--entrypoint=', '-v', '/var/run/docker.sock:/var/run/docker.sock', 'localstack/localstack', 'bin/localstack', 'status'])
+    console.log(result)
+    setStatus(result.stdout ? result.stdout : result.stderr);
+  }
+
+  const start = async () => {
+    ddClient.docker.cli.exec('run', ['--rm', '-i', '-e', 'LOCALSTACK_VOLUME_DIR=/tmp', '--entrypoint=', '-v', '/var/run/docker.sock:/var/run/docker.sock', 'localstack/localstack', 'bin/localstack', 'start', '-d'], {
+      stream: {
+        onOutput(data): void {
+          console.log(data)
+          if (data.stderr) {
+            console.log(data.stderr)
+          }
+          if(data.stdout){
+            setStatus(status.concat(data.stdout));
+          }
+          checkStatus();
+        },
+        onError(error: any): void {
+          ddClient.desktopUI.toast.error('An error occurred');
+          console.log(error)
+
+        },
+        onClose(exitCode: number): void {
+          console.log("onClose with exit code " + exitCode);
+        },
+      },
+    });
+  }
+
+
+  const stop = async () => {
+    ddClient.docker.cli.exec('run',
+      ['--rm', '-i', '-e',
+        'LOCALSTACK_VOLUME_DIR=~/.cache/localstack/volume', '--entrypoint=', '-v',
+        '/var/run/docker.sock:/var/run/docker.sock', 'localstack/localstack', 'bin/localstack', 'stop'
+      ]).then(_res => checkStatus());
+  }
+
 
   return (
     <>
-      <Typography variant="h3">Docker extension demo</Typography>
-      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
-        This is a basic page rendered with MUI, using Docker's theme. Read the
-        MUI documentation to learn more. Using MUI in a conventional way and
-        avoiding custom styling will help make sure your extension continues to
-        look great as Docker's theme evolves.
-      </Typography>
-      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
-        Pressing the below button will trigger a request to the backend. Its
-        response will appear in the textarea.
-      </Typography>
-      <Stack direction="row" alignItems="start" spacing={2} sx={{ mt: 4 }}>
-        <Button variant="contained" onClick={fetchAndDisplayResponse}>
-          Call backend
+      <Box>
+        <Button variant="contained" onClick={() => checkStatus()}>
+          Refresh
         </Button>
-
+        <Button variant="contained" onClick={start} style={{ margin: 10 }}>
+          Start LocalStack
+        </Button>
+        <Button variant="contained" onClick={stop}>
+          Stop LocalStack
+        </Button>
+      </Box>
+      <Card>
         <TextField
-          label="Backend response"
-          sx={{ width: 480 }}
-          disabled
+          label="Status"
           multiline
+          fullWidth
+          value={status}
           variant="outlined"
-          minRows={5}
-          value={response ?? ''}
         />
-      </Stack>
+      </Card>
     </>
   );
 }
