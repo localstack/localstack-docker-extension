9a653d842fa50444233142b0da24645d99f181b3
Pive01 <lucapivetta01@gmail.com>
2023-04-06 09:06:06 +0200

    Fixed bug that caused users not showing up

--- ui/src/services/util/format.ts
+++ ui/src/services/util/format.ts
@@ -6,7 +6,7 @@ export function removeNullBytes(str: string): string {
   return str.split('').filter(char => char.codePointAt(0)).join('');
 }
 
-const EXCLUDED_WSL = ['docker-desktop', 'docker-desktop-data'];
+const EXCLUDED_OS_WSL = ['docker-desktop', 'docker-desktop-data'];
 
 export function getOSsFromBinary(res: string): string[] {
   return res.split('\n').slice(3, -1) // get only the wsl items
@@ -14,13 +14,16 @@ export function getOSsFromBinary(res: string): string[] {
       .slice(0, -2)) // remove status and final /r
     .sort((a, b) => b.length - a.length) // put the selected OS as first of the list (it has * in front)
     .map(distro => distro.slice(-1).pop()) // get only the name as string of the distro found (ex. [["*","Ubuntu"],["Fedora"]] => ["Ubuntu","Fedora"])
-    .filter(distro => !EXCLUDED_WSL.includes(distro));
+    .filter(distro => !EXCLUDED_OS_WSL.includes(distro));
 }
+
+const EXCLUDED_USER_WSL = ['.', '..', 'bytes'];
 
 export function getUsersFromBinaryWindows(res: string): string[] {
   return res.split('\n').slice(5, -2) // get only directories rows
     .map(str => str.split(' ').at(-1) // get only dir name (ex "luca\r")
-      .slice(0, -1)); // remove newline 
+      .slice(0, -1)) // remove newline 
+    .filter(user => !EXCLUDED_USER_WSL.includes(user) && user.length>0);
 }
 
 export function getUsersFromBinaryUnix(res: string): string[] {
