b6281ec992693dcfba0d4321de2e32cab5b93b29
Luca Pivetta <36865043+Pive01@users.noreply.github.com>
2023-04-13 18:12:08 +0200

    fix(missing user): changed logic to get users (#33)
    * Fixed bug that caused users not showing up
    
    * moved to a more reliable approach
    
    * Update ui/src/services/util/format.ts
    
    Co-authored-by: Waldemar Hummer <waldemar.hummer@gmail.com>
    
    ---------
    
    Co-authored-by: Waldemar Hummer <waldemar.hummer@gmail.com>

--- ui/src/services/util/format.ts
+++ ui/src/services/util/format.ts
@@ -6,7 +6,7 @@ export function removeNullBytes(str: string): string {
   return str.split('').filter(char => char.codePointAt(0)).join('');
 }
 
-const EXCLUDED_WSL = ['docker-desktop', 'docker-desktop-data'];
+const EXCLUDED_OS_WSL = ['docker-desktop', 'docker-desktop-data'];
 
 export function getOSsFromBinary(res: string): string[] {
   return res.split('\n').slice(3, -1) // get only the wsl items
@@ -14,13 +14,16 @@ export function getOSsFromBinary(res: string): string[] {
       .slice(0, -2)) // remove status and final /r
     .sort((a, b) => b.length - a.length) // put the selected OS as first of the list (it has * in front)
     .map(distro => distro.slice(-1).pop()) // get only the name as string of the distro found (ex. [["*","Ubuntu"],["Fedora"]] => ["Ubuntu","Fedora"])
-    .filter(distro => !EXCLUDED_WSL.includes(distro));
+    .filter(distro => !EXCLUDED_OS_WSL.includes(distro));
 }
 
+const EXCLUDED_USER_WSL = ['[.]', '[..]', '\r'];
+
 export function getUsersFromBinaryWindows(res: string): string[] {
-  return res.split('\n').slice(5, -2) // get only directories rows
-    .map(str => str.split(' ').at(-1) // get only dir name (ex "luca\r")
-      .slice(0, -1)); // remove newline 
+  return res.split('\n').filter(string => string.startsWith('[')) // get only directory rows (they are in form "[user1] [user2]" )
+    .map(elem =>elem.split(' ').filter(item => item.length > 0 && !EXCLUDED_USER_WSL.includes(item)))// get only dir names
+    .reduce((accumulator, currentValue) => accumulator.concat(currentValue), []) // combine multiple lines into one
+    .map(user => user.slice(1, -1)); // remove "[" and "]" from names
 }
 
 export function getUsersFromBinaryUnix(res: string): string[] {
